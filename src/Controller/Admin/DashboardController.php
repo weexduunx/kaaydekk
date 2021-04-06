2ead07df65670fe609437a95ff704236d597acfd
Idrissa Ndiouck <wduunx7@gmail.com>
2021-04-06 22:34:58 +0000

    Personnalisation de Easy Admin, chagngement de layout, Menu, TopBar et content

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -15,13 +15,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-
-    public function configureAssets(): Assets
-    {
-        return Assets::new()
-            // ...
-            ->addHtmlContentToHead('<style>:root { --color-primary: #2B65EC; }</style>');
-    }
     /**
      * @Route("/admin", name="admin")
      */
@@ -34,38 +27,38 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Kaay Dëkk Immo');
+            ->setTitle('Kaay Dëkk Immo')
+             ->setTranslationDomain('/');
     }
 
     public function configureMenuItems(): iterable
     {
+              return [
+                  MenuItem::linktoDashboard('Tableau de bord', 'fa fa-home'),
+                  MenuItem::section('GESTION','fa fa-folder'),
+                  MenuItem::subMenu('Gestion des Achats')->setSubItems([
+                      MenuItem::linkToCrud('Liste des Achats', 'fas fa-list', Achat::class),
+                      MenuItem::linkToCrud('Ajouter un Achat','fas fa-plus', Achat::class)
+                          ->setAction('new'),
+                  ]),
+                  MenuItem::subMenu('Gestion des Biens')->setSubItems([
+                      MenuItem::linkToCrud('Liste des biens', 'fas fa-list', Bien::class),
+                      MenuItem::linkToCrud('Ajouter un bien', 'fas fa-plus', Bien::class)
+                          ->setAction('new'),
+                  ]),
+                  MenuItem::subMenu('Gestion des Clients')->setSubItems([
+                      MenuItem::linkToCrud('liste des clients', 'fas fa-list', Client::class),
+                      MenuItem::linkToCrud('Ajouter un client', 'fas fa-plus', Client::class)
+                          ->setAction('new'),
+                  ]),
+                  MenuItem::section('PARAMÈTRES','fa fa-cogs'),
+                  MenuItem::subMenu('Gestion des Utilisateurs', 'fas fa-user')->setSubItems([
+                      MenuItem::linkToCrud('liste des utilisateurs', 'fas fa-list', User::class),
+                      MenuItem::linkToCrud('Ajouter un utilisateur', 'fas fa-plus', User::class)
+                          ->setAction('new'),
+                  ]),
 
-        return [
-             MenuItem::linktoDashboard('Tableau de bord', 'fa fa-home'),
-             MenuItem::section('GESTION'),
-             MenuItem::subMenu('Gestion des Achats')->setSubItems([
-                 MenuItem::linkToCrud('Liste des Achats', 'fas fa-list', Achat::class),
-                 MenuItem::linkToCrud('Ajouter un Achat','fas fa-plus', Achat::class)
-                     ->setAction('new'),
-            ]),
-            MenuItem::subMenu('Gestion des Biens')->setSubItems([
-                 MenuItem::linkToCrud('Liste des biens', 'fas fa-list', Bien::class),
-                 MenuItem::linkToCrud('Ajouter un bien', 'fas fa-plus', Bien::class)
-                ->setAction('new'),
-            ]),
-            MenuItem::subMenu('Gestion des Clients')->setSubItems([
-                 MenuItem::linkToCrud('liste des clients', 'fas fa-list', Client::class),
-                 MenuItem::linkToCrud('Ajouter un client', 'fas fa-plus', Client::class)
-                ->setAction('new'),
-            ]),
-            MenuItem::section('Paramétres'),
-            MenuItem::subMenu('Gestion des Utilisateurs', 'fas fa-user')->setSubItems([
-                 MenuItem::linkToCrud('liste des utilisateurs', 'fas fa-list', User::class),
-                 MenuItem::linkToCrud('Ajouter un utilisateur', 'fas fa-plus', User::class)
-                ->setAction('new'),
-            ]),
-
-        ];
+              ];
 
     }
 }
